e0dd672db4ee478f0caf5f9fd3bcaf7ed957e045
reeechart <ferdinandusrichard@yahoo.co.id>
2019-04-02 11:41:29 +0700

    refactor package and add pickcontext boilerplate activity

---
+++ app/src/main/java/com/reeechart/ricomusic/activities/PickContextActivity.kt
@@ -0,0 +1,12 @@
+package com.reeechart.ricomusic.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class PickContextActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pick_context)
+    }
+}
