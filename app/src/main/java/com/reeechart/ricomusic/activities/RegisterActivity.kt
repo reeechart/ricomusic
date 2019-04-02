14b0905c0456eaa397843c268163e5b53438cadd
reeechart <ferdinandusrichard@yahoo.co.id>
2019-04-03 01:29:17 +0700

    add register activity

---
+++ app/src/main/java/com/reeechart/ricomusic/activities/RegisterActivity.kt
@@ -0,0 +1,13 @@
+package com.reeechart.ricomusic.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.reeechart.ricomusic.R
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
