e6cc64c983eb49ceee100b42fae6fc9ffb64eb53
reeechart <ferdinandusrichard@yahoo.co.id>
2019-04-03 01:46:07 +0700

    add link from login to register

--- app/src/main/java/com/reeechart/ricomusic/activities/LoginActivity.kt
+++ app/src/main/java/com/reeechart/ricomusic/activities/LoginActivity.kt
@@ -1,7 +1,9 @@
 package com.reeechart.ricomusic.activities
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.reeechart.ricomusic.R
 
 class LoginActivity : AppCompatActivity() {
@@ -9,5 +11,10 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
+
+    fun register(view: View) {
+        val registerIntent = Intent(this, RegisterActivity::class.java)
+        this.startActivity(registerIntent)
     }
 }
