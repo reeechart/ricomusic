5ad16024a5342619a4920037bce57a521726b8a3
reeechart <ferdinandusrichard@yahoo.co.id>
2019-04-05 22:54:14 +0700

    able to show location details after changing loc

--- app/src/main/java/com/reeechart/ricomusic/fragments/ProfileFragment.kt
+++ app/src/main/java/com/reeechart/ricomusic/fragments/ProfileFragment.kt
@@ -4,9 +4,11 @@ import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.content.ContextCompat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
 import android.widget.TextView
 import com.reeechart.ricomusic.R
 import kotlinx.android.synthetic.main.fragment_profile.view.*
@@ -23,19 +25,27 @@ class ProfileFragment: Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val rootView = inflater.inflate(R.layout.fragment_profile, container, false)
-        setLocationToView(rootView.locationName)
+        setLocationToView(rootView.locationName, rootView.locationIcon)
         return rootView
     }
 
     override fun onResume() {
         super.onResume()
-        setLocationToView(view!!.locationName)
+        setLocationToView(view!!.locationName, view!!.locationIcon)
     }
 
-    fun setLocationToView(view: TextView) {
+    fun setLocationToView(locationNameLabel: TextView, locationIcon: FrameLayout) {
         val preferences: SharedPreferences = this.activity.getSharedPreferences(getString(R.string.app_name), Context.MODE_PRIVATE)
         val location: String = preferences.getString("location", null)
 
-        view.text = location
+        when(location) {
+            getString(R.string.gym) -> locationIcon.background = ContextCompat.getDrawable(this.activity, R.drawable.ic_location_gym)
+            getString(R.string.office) -> locationIcon.background = ContextCompat.getDrawable(this.activity, R.drawable.ic_location_office)
+            getString(R.string.canteen) -> locationIcon.background = ContextCompat.getDrawable(this.activity, R.drawable.ic_location_canteen)
+            getString(R.string.library) -> locationIcon.background = ContextCompat.getDrawable(this.activity, R.drawable.ic_location_library)
+            getString(R.string.travel) -> locationIcon.background = ContextCompat.getDrawable(this.activity, R.drawable.ic_location_travel)
+        }
+
+        locationNameLabel.text = location
     }
 }
