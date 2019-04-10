b8376ad0ba27756c4988b7e66daba367aed11e47
reeechart <ferdinandusrichard@yahoo.co.id>
2019-04-11 00:27:11 +0700

    register button directs back to login page

--- app/src/main/java/com/reeechart/ricomusic/activities/RegisterActivity.kt
+++ app/src/main/java/com/reeechart/ricomusic/activities/RegisterActivity.kt
@@ -1,7 +1,9 @@
 package com.reeechart.ricomusic.activities
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.reeechart.ricomusic.R
 
 class RegisterActivity : AppCompatActivity() {
@@ -9,5 +11,9 @@ class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+    }
+
+    fun register(view: View) {
+        this.finish()
     }
 }
