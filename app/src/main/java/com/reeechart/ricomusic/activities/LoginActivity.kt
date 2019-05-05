77de5cb327942269850acd00bf3b594eaf05554c
reeechart <ferdinandusrichard@yahoo.co.id>
2019-05-05 23:25:18 +0700

    add login functionality

--- app/src/main/java/com/reeechart/ricomusic/activities/LoginActivity.kt
+++ app/src/main/java/com/reeechart/ricomusic/activities/LoginActivity.kt
@@ -6,11 +6,21 @@ import android.content.SharedPreferences
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import com.reeechart.ricomusic.R
+import com.reeechart.ricomusic.network.ricommender.RicommenderApiService
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_login.*
-import kotlinx.android.synthetic.main.fragment_profile.*
 
 class LoginActivity : AppCompatActivity() {
+
+    private var disposable: Disposable? = null
+
+    private val ricommenderApiCaller by lazy {
+        RicommenderApiService.create()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,12 +33,44 @@ class LoginActivity : AppCompatActivity() {
     }
 
     fun login(view: View) {
+        val username = usernameField.text.toString()
+        disposable = ricommenderApiCaller.login(username)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        { result ->
+                            setButtonStatusLoggingIn()
+                            commitUsername(result.username)
+                            startPickingLocation()
+                        },
+                        { error ->
+                            Toast.makeText(this, error.message, Toast.LENGTH_SHORT).show()
+                            setButtonStatusErrorLogin()
+                        }
+                )
+    }
+
+    private fun setButtonStatusLoggingIn() {
+        loggingInProgressBar.visibility = View.VISIBLE
+        loginButton.isEnabled = false
+        registerButton.isEnabled = false
+    }
+
+    private fun setButtonStatusErrorLogin() {
+        loggingInProgressBar.visibility = View.INVISIBLE
+        loginButton.isEnabled = true
+        registerButton.isEnabled = true
+    }
+
+    private fun commitUsername(username: String) {
         val preferences: SharedPreferences = getSharedPreferences(getString(R.string.app_name), Context.MODE_PRIVATE)
         preferences.edit().apply {
-            putString("username", usernameField.text.toString())
+            putString("username", username)
             commit()
         }
+    }
 
+    private fun startPickingLocation() {
         val pickLocationIntent = Intent(this, PickLocationActivity::class.java)
         this.startActivity(pickLocationIntent)
         this.finish()
---
+++ app/src/main/java/com/reeechart/ricomusic/network/ricommender/RicommenderApiService.kt
@@ -0,0 +1,48 @@
+package com.reeechart.ricomusic.network.ricommender
+
+import com.reeechart.ricomusic.models.AuthResponse
+import com.reeechart.ricomusic.models.RecommendationResponse
+import io.reactivex.Observable
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.*
+
+/**
+ * Created by Reeechart on 06-Apr-19.
+ */
+interface RicommenderApiService {
+    @GET("/music/recommendation")
+    fun getRecommendation(@Query("n") n: Int,
+                           @Query("user") user: String,
+                           @Query("loc") location: String,
+                           @Query("weather") weather: String):
+            Observable<RecommendationResponse.Result>
+
+    @POST("/history/add")
+    fun addHistory(@Field("user") user: String,
+                   @Field("music") music: Int,
+                   @Field("location") location: String,
+                   @Field("weather") weather: String,
+                   @Field("music_rank") musicRank: Int)
+
+    @GET("/user/login/{username}")
+    fun login(@Path(value="username", encoded=true) username: String): Observable<AuthResponse.Result>
+
+    @POST("/user/register")
+    fun register(@Field("username") username: String)
+
+    companion object {
+        private val BASE_URL: String = "http://157.230.243.204"
+
+        fun create(): RicommenderApiService {
+            val retrofit = Retrofit.Builder()
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .baseUrl(BASE_URL)
+                    .build()
+
+            return retrofit.create(RicommenderApiService::class.java)
+        }
+    }
+}
