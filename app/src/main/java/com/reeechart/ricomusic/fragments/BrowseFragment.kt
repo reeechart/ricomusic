b62ce18c23d461df2004a76549c27e744f06b451
reeechart <ferdinandusrichard@yahoo.co.id>
2019-05-12 00:31:18 +0700

    add recommendation calculation text randomizer while fetching

--- app/src/main/java/com/reeechart/ricomusic/fragments/BrowseFragment.kt
+++ app/src/main/java/com/reeechart/ricomusic/fragments/BrowseFragment.kt
@@ -8,6 +8,7 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.reeechart.ricomusic.R
 import com.reeechart.ricomusic.controllers.BrowseFragmentController
 
@@ -17,6 +18,7 @@ import com.reeechart.ricomusic.controllers.BrowseFragmentController
 
 class BrowseFragment: Fragment() {
     lateinit var recommendationList: RecyclerView
+    lateinit var recommendationCalculationText: TextView
     lateinit var forYouSection: ConstraintLayout
     lateinit var fetchingRecommendationProgress: ConstraintLayout
     private lateinit var controller: BrowseFragmentController
@@ -39,9 +41,22 @@ class BrowseFragment: Fragment() {
         val rootView = inflater.inflate(R.layout.fragment_browse, container, false)
         forYouSection = rootView.findViewById(R.id.forYouSection)
         fetchingRecommendationProgress = rootView.findViewById(R.id.fetchingRecommendationProgress)
+        recommendationCalculationText = rootView.findViewById(R.id.recommendationCalculationText)
+        randomizeRecommendationCalculationText()
         recommendationList = rootView.findViewById(R.id.recommendationList)
         linearLayoutManager = LinearLayoutManager(this.context)
         recommendationList.layoutManager = linearLayoutManager
         return rootView
+    }
+
+    private fun randomizeRecommendationCalculationText() {
+        val randomNumber: Int = (1..5).shuffled().first()
+        recommendationCalculationText.text = when (randomNumber) {
+            1 -> getString(R.string.recommendation_is_on_its_way)
+            2 -> getString(R.string.wait_recommendation_is_coming)
+            3 -> getString(R.string.standby_and_get_your_recommendation)
+            4 -> getString(R.string.multiplying_this_and_this_and_that)
+            else -> getString(R.string.packaging_your_recommendation)
+        }
     }
 }
